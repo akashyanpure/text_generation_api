05f6a8c70f605b5ec4ad95b46f256e6d6f4d3d3c
akashyanpure <31641763+akashyanpure@users.noreply.github.com>
2021-04-15 14:05:03 +0100

    commit

--- app.py
+++ app.py
@@ -1,11 +1,39 @@
-from flask import Flask, jsonify 
+from flask import Flask, redirect, url_for, render_template,jsonify
 from flask.globals import request
+from transformers import pipeline
+import pickle as pickle
+import tensorflow as tf
+from pickle import dump
 
-app = Flask(__name__)
+filename = 'finalized_model.sav'
+# model = pickle.load(open(filename, 'rb'))
+model = tf.keras.models.load_model('saved_models/my_model')
+print(model)
+if True:
+    print("asdf") 
+else:
+    generator = pipeline('text-generation', model='EleutherAI/gpt-neo-1.3B')
+    dump(model, open('finalized_model.sav', 'wb'))
 
-@app.route('/',methods=['POST'])
+app = Flask(__name__,template_folder='../template')
+
+languages = [{'name': 'Javascript'}]
+@app.route("/")
 def home():
-    return jsonify(data="Hey")
+    return jsonify("data")
 
+@app.route("/login",methods = ["POST"])
+def login():
+    language = {'name': request.json['name']}
+    languages.append(language)
+    return jsonify({'languages': languages})
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
+    
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+
+# joblib.dump(grid.best_estimator_, 'filename.pkl', compress = 1)
+# generator("Matt has", do_sample=True, min_length=50)
